69249dd07408945eb35ca4fa4299ac5b1bb45de2
rajwaw <541241165@student.smktelkom-pwt.sch.id>
2025-03-10 15:01:54 +0700

    config.php

--- config.php
+++ config.php
@@ -1,32 +1,14 @@
 <?php
-$host = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$database = "portofolio_pts";
+$host ='localhost';
+$user ='root';
+$pass ='';
+$db_name ='portofolio_pts';
 
-$conn = new mysqli($host, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+$conn = mysqli_connect($host,$user,$pass,$db_name);
+
+if($conn->connect_errno){
+    echo "Koneksi Gagal : " . $conn->connect_error;
+    die("eror!");
 }
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullname = $_POST["fullname"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-
-    $sql = "INSERT INTO messages (fullname, email, message) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $fullname, $email, $message);
-
-    if ($stmt->execute()) {
-        echo "<script>alert('Data berhasil dikirim!');</script>";
-    } else {
-        echo "<script>alert('Gagal mengirim data!');</script>";
-    }
-    $stmt->close();
-}
-
-$conn->close();
 ?>
